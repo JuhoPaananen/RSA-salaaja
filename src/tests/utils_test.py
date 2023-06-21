638539f59a4b02776afddb7c9718dcca3027faae
Juho <JuhoPaananen@users.noreply.github.com>
2023-06-21 11:51:11 +0300

    Päivitä testikattavuus

--- src/tests/utils_test.py
+++ src/tests/utils_test.py
@@ -8,11 +8,12 @@ class TestPrimeNumberTools(unittest.TestCase):
     def setUp(self):
         self.filter_primes = _eratosthene_sieve(10000)
         self.non_primes = [i for i in range(3,10000, 2) if i not in self.filter_primes]
+        self.large_primes = [2**607-1, 2**521-1, 2**1279-1] # source: https://en.wikipedia.org/wiki/Largest_known_prime_number
         # Using RSA library to generate 2048 bit non-prime numbers as N in keys is a product of two primes p*q
         self.non_prime_n = []
         for _ in range(10):
             self.non_prime_n.append(rsa.newkeys(2048)[0].n)
-        self.large_primes = [2**607-1, 2**521-1] # source: https://en.wikipedia.org/wiki/Largest_known_prime_number
+            self.non_prime_n.append(self.large_primes[0] * self.large_primes[1])
 
     def test_eratosthene_sieve(self):
         self.assertEqual(_eratosthene_sieve(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
